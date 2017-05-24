73987c6e1cb2b331b44d7f35f9b69f220795301e
MarcosCalvi <marcos.calvi@hotmail.com>
2017-05-24 16:34:06 +0200

    Update 5_24_17

--- build/config.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-if (b4w.module_check("config"))
-    throw "Failed to register module: config";
-
-b4w.register("config", function(exports, require) {
-	
-});
--- character.js
+++ character.js
@@ -8,24 +8,33 @@ b4w.register("character", function(exports, require) {
 var m_fps 	= require("fps");
 var m_scs 	= require("scenes");
 var m_cons  = require("constraints");
+var m_trans = require("transform");
+var m_phy 	= require("physics");
 
 var _vec3_tmp = new Float32Array(3);
 
 var _char_phys_body;
 var _char_model;
+
+var _quat = new Float32Array(4); 
 	
 var move_cb = function(forw_back, right_left) {
     //console.log(forw_back, right_left);
 	console.log("Model_position: " + _char_model.physics.curr_tsr);
 	console.log("Phys_body_position: " + _char_phys_body.physics.curr_tsr);
 	
-}	
+}
+	
 exports.init_character = function() {
-	_char_phys_body = m_scs.get_object_by_name("Character");
-	_char_model = m_scs.get_object_by_name("CharModel");
+	var spawner = m_scs.get_object_by_name("EmptySpawner1");
+	var pos = m_trans.get_translation(spawner);
+	m_trans.get_rotation(spawner, _quat);
+	_char_phys_body = m_scs.get_object_by_dupli_name("GroupCharacterJim", "Character");
+	_char_model = m_scs.get_object_by_dupli_name("GroupCharacterJim", "CharModel");	
+	_vec3_tmp = [0.0, -3.0, -0.85];
+	m_cons.append_stiff(_char_model, _char_phys_body, _vec3_tmp);
+	m_phy.set_transform(_char_phys_body, [pos[0], pos[1], pos[2]], _quat);
 	m_fps.enable_fps_controls(_char_phys_body, null, move_cb);
-	_vec3_tmp = [0.0, 6.0, 0.0];
-	m_cons.append_semi_stiff(_char_model, _char_phys_body, _vec3_tmp);
 }
 
 
