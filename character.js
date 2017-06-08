934325efc122db0e84e68213ed1687d21d0b2c5a
MarcosCalvi <marcos.calvi@hotmail.com>
2017-06-08 13:27:48 +0200

    Preparing to refactor

--- character.js
+++ character.js
@@ -25,16 +25,21 @@ var move_cb = function(forw_back, right_left) {
 	
 }
 	
-exports.init_character = function() {
-	var spawner = m_scs.get_object_by_name("EmptySpawner1");
-	var pos = m_trans.get_translation(spawner);
-	m_trans.get_rotation(spawner, _quat);
+exports.init_character = function() {	
 	_char_phys_body = m_scs.get_object_by_dupli_name("GroupCharacterJim", "Character");
 	_char_model = m_scs.get_object_by_dupli_name("GroupCharacterJim", "CharModel");	
-	_vec3_tmp = [0.0, -3.0, -0.85];
-	m_cons.append_stiff(_char_model, _char_phys_body, _vec3_tmp);
-	m_phy.set_transform(_char_phys_body, [pos[0], pos[1], pos[2]], _quat);
+	var char_offset = [0.0, -3.0, -0.85];//[0.0, 0.0, 0.0]
+	m_cons.append_stiff(_char_model, _char_phys_body, char_offset);	
 	m_fps.enable_fps_controls(_char_phys_body, null, move_cb);
+	spawn_character("EmptySpawner1");
+}
+
+var spawn_character = function(spawner_name) {
+	var spawner = m_scs.get_object_by_name(spawner_name);
+	var pos = m_trans.get_translation(spawner);
+	m_trans.get_rotation(spawner, _quat);
+	console.log("Spawner rotation: " + _quat);
+	m_phy.set_transform(_char_phys_body, [pos[0], pos[1], pos[2]], _quat);
 }
 
 
